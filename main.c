3d6aeb9f388669ff1643cec0361597b1cddd4b67
Oussama Elkharraf <ouelkhar@c2r1p1.1337.ma>
2025-05-21 22:37:59 +0100

    lexing_quots

--- main.c
+++ main.c
@@ -1,5 +1,37 @@
 #include "minishell.h"
 
+// int	quote_type(char	q)
+// {
+// 	if (q == '\'')
+// 		return (1);
+// 	else if (q == '\"')
+// 		return (2);
+// 	else
+// 		return (0);
+// }
+
+void	write_error(int	n)
+{
+	if (n == 1)
+		write(2, "Error in Parsing\n", 17);
+	else
+		write(2, "Error\n", 6);
+}
+
+void	free_tokens(t_token *token)
+{
+	t_token	*tmp;
+
+	if (!token)
+		return ;
+	while (token)
+	{
+		tmp = token->next;
+		free(token);
+		token = tmp;
+	}
+}
+
 int f_strlen(char *s)
 {
 	int i;
@@ -12,61 +44,88 @@ int f_strlen(char *s)
 	return (i);
 }
 
-int f_isspace(char *s)
+int f_isspace(char c)
 {
-    if (s)
+	return (c == ' ' || (c >= 9 && c <= 13));
+}
+
+int	is_quote(char quote)
+{
+	if (quote == '\'' || quote == '\"')
+		return (1);
+	return (0);
+}
+
+// find the closing quote & return index after it
+int	inside_quote(char *input, int start)
+{
+	char	quote;
+	int		q;
+
+	q = 0;
+	quote = input[start];						// save the opening cote
+	start++;
+	// printf("(inside quote) input = [%c]\n", input[start]);
+	while (input[start])
+	{
+	// printf("(while) input = [%c]\n", input[start]);
+		if (input[start] == quote && !q)			// found match closing quote
+		{
+			// printf("(found match [quote = %c] == [input[%c]]\n", quote, input[start]);
+			q = 1;
+			return (start + 1);
+		}
+		start++;
+	}
+	return (-1);								// no match
+}
+
+
+int	lexer_input(char *input)                // not finished
+{
+    int	i;
+	// checking closed quotes
+	// inside quots ("hello world") => one word
+	// "word  > ("") not closed error.
+    i = 0;
+   
+    while (input[i])
     {
-	    while (*s)
-	    {
-		    if (*s == ' ' || (*s >= 9 && *s <= 13))
-			    return (0);
-		    s++;
-	    }
+		// printf("(inside lexer) input = [%c]\n", input[i]);
+        if (input[i] && f_isspace(input[i]))		// skip white space if not quote
+            i++;
+        else if (is_quote(input[i]))			// handle quotes errors
+		{
+			// printf(" when found quote [i= %d]\n", i);
+			i = inside_quote(input, i);
+			// printf(" after quote [i = %d]\n", i);
+			if (i == -1)
+				return (0);
+		}
+		else
+			i++;
     }
+	// printf("Extracted token: [%.*s]\n", i , &input[i]);
+	// return all (words, quoted_string, symbols) separated.
 	return (1);
 }
 
-char	*lexer_input(char *input)                // not finished
+int parsing_function(t_token  **token, char *input)			// not finished
 {
-	char	*new_input;
-    int     i;
-    int     len;
+	// int     count;
 
-    len = f_strlen(input);
-    new_input = malloc(len + 1);
-    if (!new_input)
-        return (NULL);
-    strncpy(new_input, input, len + 1);
-    i = 0;
-    while (new_input[i])
-    {
-        if (new_input[i] && f_isspace(new_input[i]))
-            i++;
-        if (is_quots(new_input[i]))                // to be implement
-			i += inside_quotes(new_input[i]);
-		if (new_input[i] == '\0')
-			break;
-    }
-	return (new_input);
-}
-
-int parsing_function(t_token  **token, char *input) // not finished
-{
-	char    *filtred;
-	int     count;
-
-	filtred = NULL;
 	if (!token || !input || !*input)
 		return (0);
-	// cat abc
-	filtred = lexer_input(input);            // to check input for error and align it
-	if (!filtred)
+
+	if (!lexer_input(input))            		// to check input for error and align it
+	{
+		printf("lexer_error\n");
 		return (0);
-	count = count_word(filtred);                // count word in input
-	add_token(*token, count);                   // add Node to struct
+	}
+	// // count = count_word(filtred);                // count word in input
+	// add_token(*token, count);                   // add Node to struct
 	return (1);
 }
-
 
 // void    ll(void)
 // {
@@ -78,11 +137,8 @@ int main(void)
 	char    *input;
 	t_token *token;
 
-	token = NULL;
 	// atexit(ll);
-    // t_token_type    a = WORD, b = HEREDOC, c = APPEND;
-    // printf("%d %d %d\n", a, b, c);
-    // printf("size of enum [%lu]\n", sizeof(a));
+	token = NULL;
 	while (1)
 	{
 		input = readline("[minishell]$> ");
@@ -100,7 +156,7 @@ int main(void)
 				// printf("add_history\n");
 			}
 			printf("input reading by readline [%s]\n", input);
-			if (parsing_function(&token, input) == 1)
+			if (parsing_function(&token, input) == 0)
 			{
 				write_error(1);         // function to return error depends on num
 				free_tokens(token);     // function to free the linked list from tokens
--- minishell.h
+++ minishell.h
@@ -2,6 +2,7 @@
 # define MINISHELL_H
 
 # include <stdio.h>
+# include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
 # include <readline/readline.h>
